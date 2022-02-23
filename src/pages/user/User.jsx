dd248c480d037573874b27ef52a6aaceb444577d
justadigit <letmecode123456@gmail.com>
2022-02-24 00:13:35 +0630

    User Data and Posts  added

--- src/pages/user/User.jsx
+++ src/pages/user/User.jsx
@@ -1,12 +1,30 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import { useEffect } from 'react';
+import { Button, Modal } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import JADE from '../../images/jade.png';
 import { useNavigate, useParams } from 'react-router-dom';
 import {
+  AddButton,
   Container,
+  Head,
+  Header,
   InfoLabel,
   InfoValue,
+  Post,
+  PostButton,
+  PostButtonGroup,
+  PostDate,
+  PostDesc,
+  PostImage,
+  PostImgCover,
+  PostInfo,
   Posts,
+  PostsTable,
+  PostTitle,
+  PostType,
+  PostVisible,
   Update,
   UserCard,
   UserInfo,
@@ -33,6 +51,12 @@ const User = () => {
     }
   }, [uId]);
 
+  //for modal
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return loading ? (
     'Loading...'
   ) : (
@@ -53,7 +77,98 @@ const User = () => {
         <Update>Update Info</Update>
       </UserCard>
 
-      <Posts>{user.email}</Posts>
+      <Posts>
+        <Head>
+          <Header>All Your Posts</Header>
+          <AddButton onClick={handleShow}>
+            <big>+</big> Add New Post
+          </AddButton>
+        </Head>
+        <PostsTable>
+          <Post>
+            <PostInfo>
+              <PostDate>Feb 10,2022</PostDate>
+              <PostTitle>Jade Ruby Diamond</PostTitle>
+              <PostDesc>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto
+                expedita commodi ea dolor cupiditate deleniti eum eius modi
+                quae? Quasi eos nam mollitia quam aspernatur non quaerat porro
+                temporibus minima . . .
+              </PostDesc>
+              <PostType>Jade</PostType>
+              <PostButtonGroup>
+                <PostVisible vkey="approve">Approved</PostVisible>
+                <PostButton color="#09aeae">Detail</PostButton>
+                <PostButton color="#044cd0">Update</PostButton>
+                <PostButton color="#d72503">Delete</PostButton>
+              </PostButtonGroup>
+            </PostInfo>
+            <PostImgCover>
+              <PostImage src={JADE} />
+            </PostImgCover>
+          </Post>
+          <Post>
+            <PostInfo>
+              <PostDate>Feb 10,2022</PostDate>
+              <PostTitle>Jade Ruby Diamond</PostTitle>
+              <PostDesc>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto
+                expedita commodi ea dolor cupiditate deleniti eum eius modi
+                quae? Quasi eos nam mollitia quam aspernatur non quaerat porro
+                temporibus minima . . .
+              </PostDesc>
+              <PostType>Jade</PostType>
+              <PostButtonGroup>
+                <PostVisible vkey="approve">Approved</PostVisible>
+                <PostButton color="#09aeae">Detail</PostButton>
+                <PostButton color="#044cd0">Update</PostButton>
+                <PostButton color="#d72503">Delete</PostButton>
+              </PostButtonGroup>
+            </PostInfo>
+            <PostImgCover>
+              <PostImage src={JADE} />
+            </PostImgCover>
+          </Post>
+          <Post>
+            <PostInfo>
+              <PostDate>Feb 10,2022</PostDate>
+              <PostTitle>Jade Ruby Diamond</PostTitle>
+              <PostDesc>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto
+                expedita commodi ea dolor cupiditate deleniti eum eius modi
+                quae? Quasi eos nam mollitia quam aspernatur non quaerat porro
+                temporibus minima . . .
+              </PostDesc>
+              <PostType>Jade</PostType>
+              <PostButtonGroup>
+                <PostVisible vkey="approve">Approved</PostVisible>
+                <PostButton color="#09aeae">Detail</PostButton>
+                <PostButton color="#044cd0">Update</PostButton>
+                <PostButton color="#d72503">Delete</PostButton>
+              </PostButtonGroup>
+            </PostInfo>
+            <PostImgCover>
+              <PostImage src={JADE} />
+            </PostImgCover>
+          </Post>
+        </PostsTable>
+      </Posts>
+
+      {/* for adding new post (modal form) */}
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Container>
   );
 };
--- src/pages/user/User.styles.js
+++ src/pages/user/User.styles.js
@@ -1,15 +1,15 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 90%;
+  width: 80%;
   margin: auto;
   display: flex;
-  align-items: center;
   justify-content: center;
 `;
 
 //User Profile ---------
 export const UserCard = styled.div`
+  height: 400px;
   background-color: white;
   padding: 40px;
   margin: 20px;
@@ -47,8 +47,108 @@ export const Update = styled.button`
 `;
 //Posts -------------------
 export const Posts = styled.div`
-  background-color: blue;
   flex: 4;
+  background-color: white;
+  padding: 40px;
+  margin: 20px;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  -webkit-box-shadow: 0px 0px 15px 5px rgba(0, 0, 0, 0.1);
+  box-shadow: 0px 0px 15px 5px rgba(0, 0, 0, 0.1);
+`;
+
+export const Head = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+export const Header = styled.h1`
+  margin-bottom: 20px;
+`;
+export const AddButton = styled.button`
+  background-color: gold;
+  padding: 5px 10px;
+  line-height: 25px;
+  border: none;
+  font-weight: 600;
+  cursor: pointer;
+`;
+
+export const PostsTable = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+`;
+export const Post = styled.div`
+  display: flex;
+  padding: 30px 0;
+  border-bottom: 1px solid #000; ;
+`;
+export const PostInfo = styled.div`
+  flex: 4;
+  display: flex;
+  flex-direction: column;
+`;
+export const PostDate = styled.span`
+  font-size: 13px;
+  color: lightgray;
+  font-weight: 600;
+  margin: 5px 0;
+`;
+export const PostTitle = styled.span`
+  font-size: 1.2em;
+  font-weight: bold;
+  margin-bottom: 5px;
+`;
+export const PostDesc = styled.div`
+  font-size: 15px;
+  padding: 15px;
+`;
+export const PostType = styled.span`
+  background-color: lightblue;
+  font-weight: 600;
+  width: 10%;
+  padding: 2px 3px;
+  display: flex;
   align-items: center;
   justify-content: center;
+  border-radius: 10px;
+`;
+export const PostImgCover = styled.div`
+  flex: 1;
+  height: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+export const PostImage = styled.img`
+  width: 80%;
+  height: 100%;
+  object-fit: cover;
+`;
+export const PostCategory = styled.div``;
+export const PostButtonGroup = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: end;
+`;
+export const PostVisible = styled.span`
+  background: ${(props) => (props.vkey === 'approve' ? '#1fae51' : 'black')};
+  color: white;
+  font-size: 8px;
+  font-weight: 600;
+  padding: 3px 5px;
+  margin: 2px 5px;
+  border-radius: 15px;
+`;
+export const PostButton = styled.button`
+  background-color: ${(props) => (props.color ? props.color : '')};
+  color: white;
+  border: none;
+  padding: 5px 15px;
+  margin: 2px 7px;
+  font-size: 13px;
+  font-weight: 600;
+  border-radius: 5px;
 `;
